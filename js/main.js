05280d5afbeb219157f1f57b22c1fb808e67f595
DrAmonov <temurxonamonov@gmail.com>
2022-11-24 21:25:20 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var userName = prompt ('Your name');
+var userAsk = confirm ('Are ready to start quiz test');
+var testf = prompt ('1.html kengaytmasi nima? \n A) hayper text markup language \n B) hayper text markus language \n C) hybred text markup language');
+var tests = prompt ('2.html pageda nechta column boladi? \n A) 12ta \n B) 16ta \n C) 6ta');
+var testt = prompt ('3.bootsrapda nechchi turdagi ranglar mavjud? \n A) 11 \n B) 6 \n C) 9');
+var testfo = prompt ('4.tailwindda justify-content: space-between; burugini berish qanday korinishda boladi \n A) justify-between \n B) content-between \n C) space-between');
+var testfi = prompt ('5.tailwindda user css qiymat kirgizish uchun qaysi papkaga code yozadi? \n A) tailwind config js \n B) node modules \n C) main css');
+var tests = prompt ('6.tailwind backgroun img qanday kiritiladi? \n A) tag ichida style ochiladi \n B)tailwind config js da qiymat kiritiladi \n C) css papka ichida yoziladi');
+
+
+
+console.log(
+    'user_first_name' + ' ' + userName + '\n' + userAsk + '\n'
+    + '1 right ansver is A ' + ' user ansver is ' + ' ' + testf + '\n'
+    + '2 right ansver is A ' + ' user ansver is ' + ' ' + tests + '\n'
+    + '3 right ansver is A ' + ' user ansver is ' + ' ' + testt + '\n'
+    + '4 right ansver is A ' + ' user ansver is ' + ' ' + testfo + '\n'
+    + '5 right ansver is A ' + ' user ansver is ' + ' ' + testfi + '\n'
+    + '6 right ansver is A ' + ' user ansver is ' + ' ' + tests
+);
